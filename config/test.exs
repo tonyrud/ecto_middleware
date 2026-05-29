c3b59dfe0f6c357fc1126011b6c81657fa6333a5
tonyrud <tonrudny@gmail.com>
2026-05-29 12:49:23 -0400

    remove sql logs in tests

--- config/test.exs
+++ config/test.exs
@@ -5,6 +5,7 @@ alias EctoMiddleware.Test.Repo
 config :ecto_middleware, Repo,
   database: Path.expand("../test/test.db", __DIR__),
   pool: Ecto.Adapters.SQL.Sandbox,
-  show_sensitive_data_on_connection_error: true
+  show_sensitive_data_on_connection_error: true,
+  log: false
 
 config :ecto_middleware, ecto_repos: [Repo]
